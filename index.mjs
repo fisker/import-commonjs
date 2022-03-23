12b4228c7c455b2581a6cf7d095e91e85532914f
fisker Cheung <lionkay@gmail.com>
2022-03-23 21:22:12 +0800

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import * as module from 'node:module'
 
-// eslint-disable-next-line node/no-deprecated-api
+// eslint-disable-next-line no/no-deprecated-api
 const createRequire = module.createRequire || module.createRequireFromPath
 
 export default (url) => createRequire(url)
